6602cbf09f29dcf437f6de0d419db016301b3074
Emil Tomczyk <emil.tomczyk@skni.umcs.pl>
2023-11-07 09:04:31 +0100

    More

--- src/py9/py9.py
+++ src/py9/py9.py
@@ -5,6 +5,8 @@ import selectors
 import socket
 import struct
 
+STR_LEN=2
+
 
 class Py9:
 
@@ -72,10 +74,10 @@ class Py9:
             self,
             sock: socket.socket,
     ) -> dict:
-        size: int = struct.unpack("<I", self._recv_n(4))
+        size: int = struct.unpack('<I', self._recv_n(4))
         buf: bytes = self._recv_n(sock, size - 4)
         operation: self.TRs = self.TRs(int(buf[0]))
-        tag: int = struct.unpack("<H", buf[1:3])
+        tag: int = struct.unpack('<H', buf[1:3])
         other_data: dict = self._parse_data(operation, buf[3:])
 
         return {
@@ -92,51 +94,216 @@ class Py9:
         match operation:
             case self.TRs.Tversion:
                 ret = {
-                    'msize': struct.unpack("<I", data[0:4]),
+                    'msize': struct.unpack('<I', data[0:4]),
                     'version': data[4:].decode(),
                 }
+
             case self.TRs.Rversion:
                 ret = {
-                    'msize': struct.unpack("<I", data[0:4]),
+                    'msize': struct.unpack('<I', data[0:4]),
                     'version': data[4:].decode(),
                 }
+
             case self.TRs.Tauth:
+                uname_len: int = struct.unpack(
+                    '<H',
+                    data[
+                        4:
+                        4 + STR_LEN
+                    ]
+                )
+                aname_len: int = struct.unpack(
+                    '<H',
+                    data[
+                        4 + STR_LEN + uname_len:
+                        4 + STR_LEN + uname_len + STR_LEN
+                    ]
+                )
                 ret = {
-                    'afid': struct.unpcak("<I", data[0:4]),
-                    
+                    'afid': struct.unpack('<I', data[0:4]),
+                    'uname': struct.unpack(
+                        '<s',
+                        data[
+                            4 + STR_LEN:
+                            4 + STR_LEN + uname_len
+                        ],
+                    ),
+                    'aname': struct.unpack(
+                        '<s',
+                        data[
+                            4 + STR_LEN + uname_len + STR_LEN:
+                            4 + STR_LEN + uname_len + STR_LEN + aname_len
+                        ]
+                    ),
                 }
+
             case self.TRs.Rauth:
-                ...
+                ret = {
+                    'aqid': struct.unpack('<s', data[0:13]),
+                }
+
             case self.TRs.Tattach:
-                ...
+                uname_len: int = struct.unpack(
+                    '<H',
+                    data[
+                        8:
+                        8 + STR_LEN
+                    ]
+                )
+                aname_len: int = struct.unpack(
+                    '<H',
+                    data[
+                        8 + STR_LEN + uname_len:
+                        8 + STR_LEN + uname_len + STR_LEN
+                    ]
+                )
+                ret = {
+                    'fid': struct.unpack('<I', data[0:4]),
+                    'afid': struct.unpack('<I', data[4:8]),
+                    'uname': struct.unpack(
+                        '<s',
+                        data[
+                            8 + STR_LEN:
+                            8 + STR_LEN + uname_len
+                        ]
+                    ),
+                    'aname': struct.unpack(
+                        '<s',
+                        data[
+                            8 + STR_LEN + uname_len + STR_LEN:
+                            8 + STR_LEN + uname_len + STR_LEN + aname_len
+                        ]
+                    ),
+                }
+
             case self.TRs.Rattach:
-                ...
+                ret = {
+                    'qid': struct.unpack('<s', data[0:13]),
+                }
+
             case self.TRs.Terror:
-                raise Exception("There is no Terror code")
+                raise Exception('There is no Terror code')
+
             case self.TRs.Rerror:
-                ...
+                ename_len: int = struct.unpack('<H', data[0:0 + STR_LEN])
+                ret = {
+                    'ename': struct.unpack(
+                        '<s',
+                        data[0 + STR_LEN:0 + STR_LEN + ename_len]
+                    ),
+                }
+
             case self.TRs.Tflush:
-                ...
+                ret = {
+                    'oldtag': struct.unpack('<H', data[0:2]),
+                }
+
             case self.TRs.Rflush:
-                ...
+                ret = {}
+
             case self.TRs.Twalk:
-                ...
+                nwname: int = struct.unpack('<H', data[8:10])
+
+                wnames: list = []
+                offset: int = 10
+                for _ in range(nwname):
+                    wname_len = struct.unpack(
+                        '<H',
+                        data[offset:offset + STR_LEN]
+                    )
+                    wname = struct.unpack(
+                        '<s',
+                        data[offset + STR_LEN:offset + STR_LEN + wname_len]
+                    )
+                    wnames.append(wname)
+                    offset += wname_len + STR_LEN
+
+                ret = {
+                    'fid': struct.unpack('<I', data[0:4]),
+                    'newfid': struct.unpack('<I', data[4:8]),
+                    'nwname': nwname,
+                    'wnames': wnames,
+                }
+
             case self.TRs.Rwalk:
-                ...
+                nwqid: int = struct.unpack('<H', data[0:2])
+
+                qids: list = []
+                offset: int = 2
+
+                for _ in range(nwqid):
+                    qid_len = struct.unpack(
+                        '<H',
+                        data[offset:offset + STR_LEN]
+                    )
+                    qid = struct.unpack(
+                        '<s',
+                        data[offset + STR_LEN:offset + STR_LEN + qid_len]
+                    )
+                    qids.append(qid)
+                    offset += qid_len + STR_LEN
+
+                ret = {
+                    'nwqid': nwqid,
+                    'qids': qids,
+                }
+
             case self.TRs.Topen:
-                ...
+                ret = {
+                    'fid': struct.unpack('<I', data[0:4]),
+                    'mode': struct.unpack('<B', data[4:5]),
+                }
+
             case self.TRs.Ropen:
-                ...
+                ret = {
+                    'qid': struct.unpack('<s', data[0:13]),
+                    'iounit': struct.unpack('<I', data[13:17]),
+                }
+
             case self.TRs.Tcreate:
-                ...
+                name_len = struct.unpack('<H', data[4:4 + STR_LEN])
+                ret = {
+                    'fid': struct.unpack('<I', data[0:4]),
+                    'name': struct.unpack(
+                        '<s',
+                        data[4 + STR_LEN:4 + STR_LEN + name_len]
+                    ),
+                    'perm': struct.unpack(
+                        '<I',
+                        data[4 + STR_LEN + name_len:4 + STR_LEN + name_len + 4]
+                    ),
+                    'mode': struct.unpack(
+                        '<B',
+                        data[
+                            4 + STR_LEN + name_len + 4:
+                            4 + STR_LEN + name_len + 4 + 1
+                        ]
+                    )
+                }
+
             case self.TRs.Rcreate:
-                ...
+                ret = {
+                    'qid': struct.decode('<s', data[0:13]),
+                    'iounit': struct.decode('<s', data[13:17]),
+                }
+
             case self.TRs.Tread:
-                ...
+                ret = {
+                    'fid': struct.decode('<I', data[0:4]),
+                    'offset': struct.decode('<Q', data[4:12]),
+                    'count': struct.decode('<I', data[12:16]),
+                }
+
             case self.TRs.Rread:
-                ...
+                count = struct.decode('<I', data[0:4])
+                ret = {
+                    'count': count,
+                    'data': struct.decode('<s', data[4:4 + count]),
+                }
+
             case self.TRs.Twrite:
                 ...
+
             case self.TRs.Rwrite:
                 ...
             case self.TRs.Tclunk:
@@ -156,7 +323,7 @@ class Py9:
             case self.TRs.Rwstat:
                 ...
             case _:
-                raise Exception("No such operation")
+                raise Exception('No such operation')
         return ret
 
     @abstractmethod
